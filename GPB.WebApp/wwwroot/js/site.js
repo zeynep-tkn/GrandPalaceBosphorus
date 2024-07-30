7053cf752337ba5485b0c5ec8dbd70bd8827f78d
Zeynep Tekin <115029576+zeynep-tkn@users.noreply.github.com>
2024-07-30 10:14:33 +0300

    Add files via upload
    www

---
+++ GPB.WebApp/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//üye ol sayfası bölümü
+document.addEventListener('DOMContentLoaded', (event) => {
+    const modal = document.getElementById('register-modal');
+    const registerBtn = document.getElementById('register-btn');
+    const closeBtn = document.getElementsByClassName('close-btn')[0];
+
+    // Register butonuna tıklanınca modalı aç
+    registerBtn.onclick = function () {
+        modal.style.display = 'block';
+    }
+
+    // Çarpı işaretine tıklanınca modalı kapat
+    closeBtn.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+    // Modal dışına tıklanınca modalı kapat
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
